e2b1a76794c85ff74035b955a03b03ae4efa3b79
Hannah Kim <hrk624@gmail.com>
2014-10-01 12:21:01 -0400

    blogs migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001155238) do
+ActiveRecord::Schema.define(version: 20141001155943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "blog_url"
+    t.string   "class"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pages", force: true do |t|
     t.datetime "created_at"
